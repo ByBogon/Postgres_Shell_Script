f1b53804a72a54f9eac56a5a03982c12f2fd87c1
BogonPark <2002qkrqhrhs@hanmail.net>
2019-12-01 17:52:14 +0900

    Delete typo in code

--- postgres_health_check.sh
+++ postgres_health_check.sh
@@ -1,7 +1,7 @@
 MASTER_PRIVATE_IP=${Master Server IP}
 USERNAME=${Username for db connection}
 PWD=${Password for db connection}
-ARCHIVE_DIR=${Absolute path with * where WAL file is saving, ex)/home/postgres/9.6/archive/*}r
+ARCHIVE_DIR=${Absolute path with * where WAL file is saving, ex)/home/postgres/9.6/archive/*}
 POSTGRES_DIR=/home/postgres/9.6
 ARCHIVE_DIR=$POSTSGRES_DIR/archive/*
 RECOVERY_CONF_DIR=$POSTGRES_DIR/data/recovery.conf
